d0d78c9569fbc138dfac0d8c18fe329949f3bb0b
Kagu7uchi <ttm3669@rit.edu>
2021-03-06 16:29:18 -0800

    yuh

--- 21/Fujin.ps1
+++ 21/Fujin.ps1
@@ -6,8 +6,8 @@ netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 netsh advfirewall firewall add rule name=”ICMP-in” dir=in action=allow protocol=icmpv4:8,any
 netsh advfirewall firewall add rule name=”ICMP-out” dir=out action=allow protocol=icmpv4:8,any
 
-netsh advfirewall firewall add rule name="RDP" dir=in protocol=tcp remoteport=3389 action=allow remoteip=10.2.1.3
-netsh advfirewall firewall add rule name="RDP" dir=out protocol=tcp remoteport=3389 action=allow remoteip=10.2.1.3
+netsh advfirewall firewall add rule name="RDP" dir=in protocol=tcp localport=3389 action=allow remoteip=10.2.1.3
+netsh advfirewall firewall add rule name="RDP" dir=out protocol=tcp localport=3389 action=allow remoteip=10.2.1.3
 
 netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in protocol=tcp localport=5985 action=allow
 netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-Out)" dir=out protocol=tcp localport=5985 action=allow
@@ -19,6 +19,8 @@ netsh advfirewall firewall add rule name="Database-in" dir=in protocol=tcp remot
 netsh advfirewall firewall add rule name="Database-out" dir=out protocol=tcp remoteport=3306 action=allow remoteip=172.16.2.11
 
 netsh advfirewall firewall add rule name="DNS" dir=out action=allow protocol=udp remoteport=53
+netsh advfirewall firewall add rule name="DNS" dir=out action=allow protocol=udp localport=53
+netsh advfirewall firewall add rule name="DNS" dir=in action=allow protocol=udp remoteport=53
 netsh advfirewall firewall add rule name="DNS" dir=in action=allow protocol=udp localport=53
 
 netsh advfirewall firewall add rule name="AD-tcp-in" dir=in action=allow protocol=tcp remoteport=389,445,88,135,3268  
--- 21/Izanagi.ps1
+++ 21/Izanagi.ps1
@@ -1,26 +1,34 @@
+# cleaning the firewall table
 netsh advfirewall reset
 netsh advfirewall set allprofiles state on
 netsh advfirewall firewall delete rule name=all
 netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 
+# ICMP
 netsh advfirewall firewall add rule name="ICMP" dir=in action=allow protocol=icmpv4:8,any
 netsh advfirewall firewall add rule name="ICMP" dir=out action=allow protocol=icmpv4:8,any
 
-netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in protocol=tcp localport=5985 action=allow
-netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-Out)" dir=out protocol=tcp localport=5985 action=allow
+# DNS 
+netsh advfirewall firewall add rule name="DNS" action=allow protocol=udp remoteport=53 dir=out 
+netsh advfirewall firewall add rule name="DNS" action=allow protocol=udp localport=53 dir=out 
+netsh advfirewall firewall add rule name="DNS" action=allow protocol=udp remoteport=53 dir=in 
+netsh advfirewall firewall add rule name="DNS" action=allow protocol=udp localport=53 dir=in 
 
-netsh advfirewall firewall add rule name="Windows Remote Management - Cloud (HTTP-In)" dir=in protocol=tcp remoteport=5985 action=allow
-netsh advfirewall firewall add rule name="Windows Remote Management - Cloud (HTTP-out)" dir=out protocol=tcp remoteport=5985 action=allow 
-
-netsh advfirewall firewall add rule name="DNS" dir=out action=allow protocol=udp localport=53 remoteport=53
-netsh advfirewall firewall add rule name="DNS" dir=in action=allow protocol=udp localport=53 remoteport=53
-
+# AD Services
 netsh advfirewall firewall add rule name="AD-tcp-in" action=allow protocol=tcp localport=389,445,88,135,3268 dir=in 
 netsh advfirewall firewall add rule name="AD-udp-in" action=allow protocol=udp localport=389,445,88,135,123 dir=in 
-
 netsh advfirewall firewall add rule name="AD-tcp-out" action=allow protocol=tcp localport=389,445,88,135,3268 dir=out
 netsh advfirewall firewall add rule name="AD-udp-out" action=allow protocol=udp localport=389,445,88,135,123 dir=out
 
+# WinRM - to scoring machines
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in protocol=tcp localport=5985 action=allow remoteip=172.16.128.0/17
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-out)" dir=out protocol=tcp localport=5985 action=allow remoteip=172.16.128.0/17
+
+# WinRM - to cloud machines
+netsh advfirewall firewall add rule name="Windows Remote Management - Cloud (HTTP-In)" dir=in protocol=tcp remoteport=5985 action=allow
+netsh advfirewall firewall add rule name="Windows Remote Management - Cloud (HTTP-out)" dir=out protocol=tcp remoteport=5985 action=allow 
+
+# IIS Remote Management - out 
 netsh advfirewall firewall add rule name="IIS Remote Management" dir=out action=allow protocol=TCP remoteport=8172
 
 netsh advfirewall set allprofiles logging filename $HOME\Pictures\fw.log
