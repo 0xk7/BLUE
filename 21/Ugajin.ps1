e03fcc196dd6767b8c53bab7f5155b0ce152b343
Kagu7uchi <ttm3669@rit.edu>
2021-03-06 16:33:00 -0800

    yuh

--- 21/Ugajin.ps1
+++ 21/Ugajin.ps1
@@ -6,13 +6,15 @@ netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 netsh advfirewall firewall add rule name=”ICMP-in” dir=in action=allow protocol=icmpv4:8,any
 netsh advfirewall firewall add rule name=”ICMP-out” dir=out action=allow protocol=icmpv4:8,any
 
-netsh advfirewall firewall add rule name="RDP" dir=in protocol=tcp remoteport=3389 action=allow remoteip=172.16.2.21
-netsh advfirewall firewall add rule name="RDP" dir=out protocol=tcp remoteport=3389 action=allow remoteip=172.16.2.21
+netsh advfirewall firewall add rule name="RDP-in" dir=in protocol=tcp remoteport=3389 action=allow remoteip=172.16.2.21
+netsh advfirewall firewall add rule name="RDP-out" dir=out protocol=tcp remoteport=3389 action=allow remoteip=172.16.2.21
 
-netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in protocol=tcp remoteport=5985 action=allow
-netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-Out)" dir=out protocol=tcp remoteport=5985 action=allow
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-In)" dir=in protocol=tcp localport=5986 action=allow remoteip=172.16.128.154
+netsh advfirewall firewall add rule name="Windows Remote Management (HTTP-Out)" dir=out protocol=tcp localport=5986 action=allow remoteip=172.16.128.154
 
 netsh advfirewall firewall add rule name="DNS" dir=out action=allow protocol=udp remoteport=53
+netsh advfirewall firewall add rule name="DNS" dir=out action=allow protocol=udp localport=53
+netsh advfirewall firewall add rule name="DNS" dir=in action=allow protocol=udp remoteport=53
 netsh advfirewall firewall add rule name="DNS" dir=in action=allow protocol=udp localport=53
 
 netsh advfirewall firewall add rule name="AD-tcp-in" dir=in action=allow protocol=tcp remoteport=389,445,88,135,3268  
